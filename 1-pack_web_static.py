07ddf15f49eff82288f268dec49f00cade1fdb85
reinhard <reinhard2057@gmail.com>
2022-12-28 20:42:24 +0000

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,15 +9,15 @@ from os import path
 
 
 def do_pack():
-	"""
-	generates achive from the web_static folder/dir
-	"""
-	try:
-		if not path.exists("versions"):
-			local('mkdir versions')
-		time_s = datetime.now().strftime("%Y%m%d%H%M%S")
-		full_path = "versions/web_static_{}.tgz".format(time_s)
-		local('tar -cvzf {} web_static'.format(full_path))
-		return full_path
-	except:
-		return None
+    """
+    generates achive from the web_static folder/dir
+    """
+    try:
+        if not path.exists("versions"):
+            local('mkdir versions')
+        time_s = datetime.now().strftime("%Y%m%d%H%M%S")
+        full_path = "versions/web_static_{}.tgz".format(time_s)
+        local('tar -cvzf {} web_static'.format(full_path))
+        return full_path
+    except Exception:
+        return None

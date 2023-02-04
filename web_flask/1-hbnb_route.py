eaf83eae79634a0d6cad58f9804194334f13e7c0
reinhard <reinhard2057@gmail.com>
2023-02-04 07:43:02 +0000

    task 0

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""
+this module just like the last
+added a new route
+"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_world():
     return 'Hello HBNB!'
 
@@ -14,4 +16,5 @@ def hbnb():
     return 'HBNB'
 
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

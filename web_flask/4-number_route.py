0b170fdb12426b2bcf11a88791ca8db8cf04c0e9
reinhard <reinhard2057@gmail.com>
2023-02-04 08:48:14 +0000

    task 0

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,33 +1,44 @@
 #!/usr/bin/python3
+"""
+this module just like the last
+added a new route
+"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_world():
+    """hello world"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """hello world"""
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def text0_func(text):
-    return f'C {text}'
+@app.route('/c/<text>', strict_slashes=False)
+def text_func(text):
+    """hello world"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python/<text>')
-def text1_func(text):
-    return f'Python {text}'
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<string:text>", strict_slashes=False)
+def python(text="is cool"):
+    """hello python"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
-@app.route('/number/<int:n>')
+
+@app.route('/number/<int:n>', strict_slashes=False)
 def text2_func(n):
-    return f'{n} is a number'
+    """numberz"""
+    return '{} is a number'.format(n)
 
 
-app.run(host='0.0.0.0', port=3000)
-
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
